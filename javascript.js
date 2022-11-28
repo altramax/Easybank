d6644bc83f4c6d367db72712b440570226239545
altramax <ajayi_ezekiel@yahoo.com>
2022-11-28 15:05:14 +0100

    update

--- javascript.js
+++ javascript.js
@@ -3,18 +3,21 @@
 const openMenuBTN = document.querySelector('.open');
 const closeMenuBTN = document.querySelector('.close');
 const menu = document.querySelector('.menu-container');
-const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
 
 openMenuBTN.addEventListener('click', function(){
     menu.classList.remove('hidden');
     openMenuBTN.classList.toggle('hidden');
     closeMenuBTN.classList.toggle('hidden');
+    overlay.classList.remove('hidden');
 })
 
 const closeMenu = function(){
     menu.classList.add('hidden');
     openMenuBTN.classList.toggle('hidden');
     closeMenuBTN.classList.toggle('hidden');
+    overlay.classList.add('hidden');
 }
 
 closeMenuBTN.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu)
